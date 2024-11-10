0c25b86e343a185c7a4b3c2bf67f9d690ce139d8
hjoll6 <doolim98@gmail.com>
2024-11-11 02:05:49 +0900

    .

--- install.sh
+++ install.sh
@@ -1,33 +1,27 @@
 #!/bin/bash -eu
 
-install_pkg (){
-    if command -v brew &>/dev/null; then
-        brew install "$1"
-    elif command -v apt-get &>/dev/null; then
-        sudo apt-get install -y "$1"
-    elif command -v pacman &>/dev/null; then
-        sudo pacman -Syu --noconfirm "$1"
-    elif command -v dnf &>/dev/null; then
-        sudo dnf install -y "$1"
-    else
-        echo "Package manager not found. Please install $1 manually."
-        exit 1
-    fi
-}
+# Initalize package manager, and define `install_pkg` function
+if command -v brew &>/dev/null; then
+    brew update
+    install_pkg (){ brew install "$1"; }
 
-init_pkg_manager (){
-    if command -v brew &>/dev/null; then
-        brew update
-    elif command -v apt-get &>/dev/null; then
-        sudo apt-get update
-    elif command -v pacman &>/dev/null; then
-        sudo pacman -Syu
-    elif command -v dnf &>/dev/null; then
-        sudo dnf check-update
-    fi
-}
+elif command -v apt-get &>/dev/null; then
+    sudo apt-get update
+    install_pkg (){ sudo apt-get install -y "$1"; }
 
-init_pkg_manager
+elif command -v pacman &>/dev/null; then
+    sudo pacman -Syu
+    install_pkg (){ sudo pacman -Syu --noconfirm "$1"; }
+
+elif command -v dnf &>/dev/null; then
+    sudo dnf check-update
+    install_pkg (){ sudo dnf install -y "$1"; }
+
+else
+    echo "Package manager not found. Please install brew, apt-get, pacman, or dnf." >&2
+    exit 1
+fi
+
 install_pkg stow
 
 # Install my .dotfiles repository
